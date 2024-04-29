85d0fcc0dcf6237d0c26659f88ac78ccbe4e6f83
LavanyaParamasivan <lavanstar27@gmail.com>
2024-04-29 11:06:28 +1000

    healthcare data

--- index.js
+++ index.js
@@ -1,80 +1,10 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const path = require("path");
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port= process.env.PORT || 8081;
 
-const app = express();
-app.use(bodyParser.json());
-/* 
-  IMPORTANT:
-    ***NEVER*** store credentials unencrypted like this.
-    This is for demo purposes only in order to simulate a functioning API serverr.
-*/
-const users = {
-  "lav@firstaid.com": {
-    firstName: "lav",
-    lastName: "p",
-    email: "lav@firstaid.com",
-    password: "very-secret",
-  },
-  "ket@firstaid.com": {
-    firstName: "ket",
-    lastName: "P",
-    email: "ket@firstaid.com.com",
-    password: "super-secret",
-  },
-};
-
-
-// use this to add a 1 second delay to all requests
-// app.use(function (req, res, next) {
-//   setTimeout(next, 1000);
-// });
-
-app.get("/api/healthDetails", (req, res) => {
-
-  res.send(healthDetailss);
-});
-
-app.post("/api/cart", (req, res) => {
-  cart = req.body;
-  setTimeout(() => res.status(201).send(), 20);
-});
-
-app.get("/api/cart", (req, res) => res.send(cart));
-
-app.post("/api/register", (req, res) =>
-  setTimeout(() => {
-    const user = req.body;
-    if (user.firstName && user.lastName && user.email && user.password) {
-      users[user.email] = user;
-      res.status(201).send({
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
-      });
-    } else {
-      res.status(500).send("Invalid user info");
-    }
-  }, 800)
-);
-
-/* IMPORTANT:
-    The code below is for demo purposes only and does not represent good security
-    practices. In a production application user credentials would be cryptographically 
-    stored in a database server and the password should NEVER be stored as plain text. 
-*/
-app.post("/api/sign-in", (req, res) => {
-  const user = users[req.body.email];
-  if (user && user.password === req.body.password) {
-    res.status(200).send({
-      userId: user.userId,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      email: user.email,
-    });
-  } else {
-    res.status(401).send("Invalid user credentials.");
-  }
-});
-
-app.listen(8081, () => console.log("API Server listening on port 8081!"));
-
+server.use(middlewares);
+server.use(router);
+server.listen(port);
